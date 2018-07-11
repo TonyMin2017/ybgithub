2953f3dd43c680b5b442c1f5c60e279e526aa459
TonyMin <1302981271@qq.com>
2018-07-11 14:59:02 +0800

    初始化代码 。

--- src/main/java/com/sz/yb/review/Test.java
+++ src/main/java/com/sz/yb/review/Test.java
@@ -6,5 +6,7 @@ package com.sz.yb.review;
 public class Test {
     public static void main(String[] args) {
         System.out.println("this is a just test...");
+        System.out.println("hehe,kaka,haha,,,");
+        System.out.println("this is very good.");
     }
 }
